eeda12f477377a1ef1f1d1a9a34872c855665514
Diego2dRamirez <diego.ramirez2m03@gmail.com>
2025-09-01 14:58:45 -0600

    midu: Modulos Nativos require vs import

---
+++ nd-js-midu-first-steps/mjs/1.os-info.mjs
@@ -0,0 +1,12 @@
+// MODULO NATIVO
+// Devuelve inforación del sitema operativo  'os'
+import { platform, release, freemem, totalmem } from 'node:os';
+
+console.log('Información del sistema operativo:');
+console.log('______________________');
+
+console.log('Nombre del sistema operativo:', platform());
+console.log('Versión del sistema operativo:', release());
+console.log('Memoria Libre:', freemem() / 1024 / 1024);
+console.log('Memoria Total:', totalmem() / 1024 / 1024);
+
