0543ca732d2c121f6456d6b17f32d49c71eb7253
Diego2dRamirez <diego.ramirez2m03@gmail.com>
2025-09-02 22:22:53 -0600

    midu: Mejorando la App | Listar archivos/directorios

--- nd-js-midu-first-steps/8.ls-advanced.js
+++ nd-js-midu-first-steps/8.ls-advanced.js
@@ -1,15 +1,41 @@
 const fs = require('node:fs/promises');
+const path = require('node:path');
 
 // Carpeta de donde queremos enlistar los archivos
 const folder = process.argv[2] ?? '.'
 
-fs.readdir(folder).then(files => {
-  files.forEach(file => {
-    console.log(file);
-  })
-}).catch(err => {
-  if (err) {
-    console.error("Error al leer el directorio", err);
-    return;
+async function ls(folder) {
+  let files
+  try {
+    files = await fs.readdir(folder)
+  } catch {
+    console.error(`No se pudeo leer el directorio ${folder}`);
+    process.exit(1);
   }
-});
+
+  const filesPromisses = files.map(async file => {
+    const filePath = path.join(folder, file);
+    let stats;
+
+    try {
+      stats = await fs.stat(filePath);
+    } catch (error) {
+      console.error(`No se puede leer el archico ${filePath}`);
+      process.exit(1)
+    }
+
+    const isDirectory = stats.isDirectory();
+    const fileType = isDirectory ? 'd' : 'f';
+    const fileSIze = stats.size;
+    const fileModified = stats.mtime.toLocaleString();
+
+    return `${fileType} ${file.padEnd(20)} ${fileSIze.toString().padStart(10)} ${fileModified}`
+  });
+
+  const filesInfo = await Promise.all(filesPromisses)
+
+  filesInfo.forEach(file => console.log(file))
+
+}
+
+ls(folder)
