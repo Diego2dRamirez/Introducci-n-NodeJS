23e7800076614a135539ae0283339a6170eaddde
Diego2dRamirez <diego.ramirez2m03@gmail.com>
2025-09-05 17:09:36 -0600

    clase-2: Headers (cabeceras), Instalación de Nodemon para actualizar el server de forma automatica

--- node-midudev/clase-2/1.http.js
+++ node-midudev/clase-2/1.http.js
@@ -1,16 +1,32 @@
 const http = require('node:http')
+const fs = require('node:fs')
 
 const desiredPort = process.env.PORT ?? 3001;
 
 // req.url --> traer información de la request y empezar a descriminar la API ---> depediendo de los headers de las rutas actuara de forma distinta
 const processRequets = (req, res) => {
   // console.log("request received", req.url);
+
+  //Cabeceras --> Headers
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+
   // creando rutas --> header 
   if (req.url === '/') {
     res.statusCode = 200 //ok
-    res.setHeader('Content-Type', 'text/html; charset=utf-8')
     res.end("<h1>Hola Node js --> Bienvenido a mi página de inicio</1>")
     // otras Rutas
+  } else if (req.url === '/image.png') {
+    fs.readFile('./OIP.png', (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end('<h1>500 Internal Server Error</h1>')
+      } else {
+        // Headers --> Cabeceras
+        res.setHeader('COntent-Type', 'image/png');
+        res.end(data)
+      }
+    })
+
   } else if (req.url === '/contacto') {
     res.statusCode = 200
     res.end("<h1>Contacto</h1>")
