41916b12653e7805b010238d63cf90ac875a3cc5
Diego2dRamirez <diego.ramirez2m03@gmail.com>
2025-09-07 22:06:43 -0600

    clase-3: Recuperar una película por su ID

--- node-midudev/clase-3/app.js
+++ node-midudev/clase-3/app.js
@@ -9,11 +9,19 @@ app.get('/', (req, res) => {
   //  res.send('¡Servidor Express activo!');
 })
 
+// Todos los recursos que sean MOVIES se identifican con /movies
 app.get('/movies', (req, res) => {
   res.json(movies)
 })
 
+// Obtención de una Movie por su ID
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params;
 
+  const movie = movies.find( movie => movie.id === id);
+  if (movie) return res.json(movie);
+  res.status(404).json({message: 'Movie not found'})
+})
 
 const PORT = process.env.PORT ?? 3001
 
