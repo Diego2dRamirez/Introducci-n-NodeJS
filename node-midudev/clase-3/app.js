a40aa3955a64be8eff4e376bf53f862dbfe2352d
Diego2dRamirez <diego.ramirez2m03@gmail.com>
2025-09-07 21:59:07 -0600

    clase-3: Agregando Json de las movies, Recuperar todas las movies con el metodo GET

--- node-midudev/clase-3/app.js
+++ node-midudev/clase-3/app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
+const movies = require('./movies.json')
+
 app.disable('x-powered-by')
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hola NodeJS + Express' })
   //  res.send('¡Servidor Express activo!');
 })
+
+app.get('/movies', (req, res) => {
+  res.json(movies)
+})
+
+
 
 const PORT = process.env.PORT ?? 3001
 
