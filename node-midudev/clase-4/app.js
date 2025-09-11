80a850ffd5664cf13bed5fb1e3b4a71323f69fce
Diego2dRamirez <diego.ramirez2m03@gmail.com>
2025-09-11 16:49:46 -0600

    clase-4: Copiando la calse-3 para implementar MVC

---
+++ node-midudev/clase-4/app.js
@@ -0,0 +1,123 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const movies = require('./movies.json')
+const crypto = require('node:crypto')
+const { validateMovie, validatePartialMovie } = require('./schemas/movieSchema')
+
+app.disable('x-powered-by')
+app.use(express.json())
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:3001',
+      'http://localhost:8080',
+      'https://movies.com',
+      'https://2d.dev'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hola NodeJS + Express' })
+  //  res.send('¡Servidor Express activo!');
+})
+
+// Todos los recursos que sean MOVIES se identifican con /movies
+// Recuperar todas las películas pero por género
+app.get('/movies', (req, res) => {
+  // Se puede obtener los query params que se coloque en el url --- /movies?genre=Terror ---
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLocaleLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
+  res.json(movies)
+})
+
+// Obtención de una Movie por su ID
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params;
+
+  const movie = movies.find(movie => movie.id === id);
+  if (movie) return res.json(movie);
+  res.status(404).json({ message: 'Movie not found' })
+})
+
+// Crear una Película
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  // base de datos
+  const newMovie = {
+    id: crypto.randomUUID(), //uuid v4
+    ...result.data // datos validados no es lo mismo que --❌req.body
+  }
+
+  // Esto no sería REST, porque se esta guardando el estado de la aplicación en memoría
+  movies.push(newMovie);
+
+  res.status(201).json(movies);
+})
+
+// Actualizar una propiedad de la película
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+
+  const movieIndex = movies.findIndex(movie => movie.id === id);
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  const upadateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = upadateMovie
+
+  return res.json(upadateMovie)
+
+})
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(400).json({ message: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+  return res.json({ message: 'Movie deleted' })
+
+})
+
+const PORT = process.env.PORT ?? 3001
+
+app.listen(PORT, () => {
+  console.log(`Server Listening on port http://loaclhost:${PORT}`);
+})
---
+++ node-midudev/clase-4/schemas/movieSchema.js
@@ -0,0 +1,34 @@
+const z = require('zod')
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Movie title must be a string',
+    required_error: 'Movie title is required.'
+  }),
+  year: z.number().int().min(1900).max(2025),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  poster: z.url({
+    message: 'Poster must be a valid URL'
+  }),
+  genre: z.array(
+    z.enum(['Action', 'Adventure', 'Comedy', 'Drama', 'Horror', 'Thriller', 'Sci-Fi', 'Crime'])
+  ),
+  rate: z.number().min(0).max(10).default(5.5)
+})
+
+function validateMovie(object) {
+  // Devuelve sí hay un error o hay datos -->safeParse
+  return movieSchema.safeParse(object)
+}
+
+function validatePartialMovie(object) {
+  // Partial --> las propiedades serán opcionales, decide si se requiere cambiar multiples propiedades o solo una... 
+  return movieSchema.partial().safeParse(object)
+}
+
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie
+}
