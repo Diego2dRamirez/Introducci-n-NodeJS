df660828afb8c6530f451f9139f56e3ea4bc6dea
Diego2dRamirez <diego.ramirez2m03@gmail.com>
2025-09-17 10:56:04 -0600

    1°-app: Servidor express

--- node-fatz/first-application-nodejs/src/index.js
+++ node-fatz/first-application-nodejs/src/index.js
@@ -1,1 +1,24 @@
-console.log("Hello NodeJS");
+import express from 'express';
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+
+const app = express();
+
+// obtener la ruta absoluta 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.set('views', join(__dirname, 'views'))
+// motor de vistas o de plantillas
+app.set('view engine', 'ejs')
+
+
+
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+
+const PORT = process.env.PORT ?? 3001
+app.listen(PORT, () => {
+  console.log(`Server Listening on port http://loaclhost:${PORT}`);
+})
