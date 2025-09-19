28b7f620724d521efd989de23d052c5f7292e4a6
Diego2dRamirez <diego.ramirez2m03@gmail.com>
2025-09-19 10:22:21 -0600

    clase-4: Controllers

--- node-midudev/clase-4/routes/movies.js
+++ node-midudev/clase-4/routes/movies.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
-import { validateMovie, validatePartialMovie } from '../schemas/movieSchema.js'
-import { MovieModel } from '../models/movie.js'
+import { MovieController } from '../controllers/moviesControllers.js'
+
 
 // import { createRequire } from 'node:module'
 // como leer un json en ESModules recomendado por ahora --> creando un método require
@@ -10,49 +10,12 @@ import { MovieModel } from '../models/movie.js'
 
 export const moviesRouter = Router()
 
-moviesRouter.get('/', async (req, res) => {
-  // Se puede obtener los query params que se coloque en el url --- /movies?genre=Terror ---
-  const { genre } = req.query
-  const movies = await MovieModel.getAll({ genre })
-  res.json(movies)
-})
+moviesRouter.get('/', MovieController.getAll)
 
-moviesRouter.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  const movie = await MovieModel.getById({ id })
-  if (movie) return res.json(movie);
-  res.status(404).json({ message: 'Movie not found' })
-})
+moviesRouter.get('/:id', MovieController.getById)
 
-moviesRouter.post('/', async (req, res) => {
-  const result = validateMovie(req.body)
-  if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-  const newMovie = await MovieModel.create({ input: result.data })
-  res.status(201).json(newMovie);
-})
+moviesRouter.post('/', MovieController.create)
 
-moviesRouter.patch('/:id', async (req, res) => {
-  const result = validatePartialMovie(req.body)
+moviesRouter.patch('/:id', MovieController.update)
 
-  if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const { id } = req.params
-
-  const upadateMovie = await MovieModel.update({ id, input: result.data })
-  return res.json(upadateMovie)
-
-})
-
-moviesRouter.delete('/:id', async (req, res) => {
-  const { id } = req.params
-  const result = await MovieModel.delete({ id })
-  if (result === -1) {
-    return res.status(400).json({ message: 'Movie not found' })
-  }
-  return res.json({ message: 'Movie deleted' })
-
-})
+moviesRouter.delete('/:id', MovieController.delete)
