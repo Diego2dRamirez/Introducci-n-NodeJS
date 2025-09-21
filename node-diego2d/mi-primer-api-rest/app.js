c6827648a6fc738559613110e00cba39c45a698f
Diego2dRamirez <diego.ramirez2m03@gmail.com>
2025-09-20 20:20:46 -0600

    mi-primer-api: GET filtrar por marca + query string

--- node-diego2d/mi-primer-api-rest/app.js
+++ node-diego2d/mi-primer-api-rest/app.js
@@ -6,6 +6,18 @@ app.disable('x-powered-by')
 app.use(express.json())
 
 app.get('/cars', (req, res) => {
+  const { marca } = req.query
+
+  if (marca) {
+    const filteredCars = cars.filter(
+      car => car.marca.toLocaleLowerCase() === marca.toLocaleLowerCase()
+    )
+
+    if (filteredCars.length === 0) {
+      res.status(404).json({ message: 'Marca no encontrada' })
+    }
+    return res.json(filteredCars)
+  }
 
   res.json(cars)
 })
