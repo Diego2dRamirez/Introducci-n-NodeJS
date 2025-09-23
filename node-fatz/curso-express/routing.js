ff30e5185b3619b67b0ba85979866c98ba870f93
Diego2dRamirez <diego.ramirez2m03@gmail.com>
2025-09-22 20:33:19 -0600

    fazt-curso-express: Routing

---
+++ node-fatz/curso-express/routing.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello express')
+})
+
+app.get('/about', (req, res) => {
+  res.send('About')
+})
+
+app.get('/weather', (req, res) => {
+  res.send('The current weather is Nice')
+})
+
+app.use((req, res) => {
+  res.status(404).send('Page not found')
+})
+
+app.listen('3000', () => {
+  console.log('Server on port 3000');
+})
